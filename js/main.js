1eef685361bc80473d511e2ae67be8f2fb23ee4c
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-01-02 20:19:33 +0530

    2 jan 2020 evening

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  /*******************Navbar Logo*********************** */
+  $(".navbar-brand").slideUp("slow");
+  // $(".navbar-brand").animate({ opacity: "1" }, 3000);
+});
