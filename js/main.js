f517263673a65c13a58456edd22fb0796112e206
Baljeet Jangra <54733764+baljeetjangra@users.noreply.github.com>
2020-01-07 21:56:39 +0530

    Preloader Added

--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  /*******************Navbar Logo*********************** */
-  // $(".navbar-brand").slideUp("slow");
-  $(".navbar-brand").animate({ opacity: "1" }, 3000);
-});
---
+++ js/preload.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  /*******************Preloader*********************** */
+  setInterval(function() {
+    $(".preload").addClass("predisplay");
+  }, 4000);
+});
